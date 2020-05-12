debf95d1b303b883357660e8c2fe8bc75b8889aa
KwandaBusoAutomationRepo <kwandabuso@gmail.com>
2020-05-12 10:09:30 +0200

    removed results

--- ILabApplication/Res22/Report/test_data_src.js
+++
@@ -1,1 +0,0 @@
-﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\kwanda.buso\\Documents\\Unified Functional Testing\\ILabApplication\\Res22',namePairs:[]}]};
